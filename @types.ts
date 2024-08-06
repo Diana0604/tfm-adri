c47111ce177163520c6ee13b8cf85ce701a0ec81
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-08-07 00:06:38 +0200

    add hint structure

--- @types.ts
+++ @types.ts
@@ -1,5 +1,16 @@
 type Character = {
   name?: string,
   surname: string,
-  title?: string
+  title?: string,
+  hintsA1: Hint[],
+  hintsA2: Hint[],
+  hintsA3: Hint[],
+  hintsA4: Hint[],
+  hintsA5: Hint[],
+}
+
+type HintType = 'Carta' | 'Foto' | 'Grabacion'
+
+type Hint = {
+  type: HintType
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,54 +1,119 @@
 export const characters: Character[] = [
   {
     name: 'J.',
-    surname: 'Cortes'
+    surname: 'Cortes',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     name: 'Lucia',
-    surname: 'Cortes'
+    surname: 'Cortes',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     name: 'Sergi',
-    surname: 'Roca'
+    surname: 'Roca',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     name: 'Pedro',
-    surname: 'Ferrer'
+    surname: 'Ferrer',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     name: 'Ernesto',
-    surname: 'Larrea'
+    surname: 'Larrea',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     surname: 'Garcia',
     title: 'Sargento',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     name: 'Marta',
-    surname: 'Moreno'
+    surname: 'Moreno',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     name: 'Abril',
-    surname: 'Curto'
+    surname: 'Curto',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     name: 'Daniel',
-    surname: 'Gutierrez'
+    surname: 'Gutierrez',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     name: 'Andrea',
-    surname: 'Marino'
+    surname: 'Marino',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     name: 'Ivette',
-    surname: 'Ferrer'
+    surname: 'Ferrer',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     name: 'Carim',
-    surname: 'Alam'
+    surname: 'Alam',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   },
   {
     name: 'Ema',
-    surname: 'Dupont'
+    surname: 'Dupont',
+    hintsA1: [],
+    hintsA2: [],
+    hintsA3: [],
+    hintsA4: [],
+    hintsA5: [],
   }
 ]
