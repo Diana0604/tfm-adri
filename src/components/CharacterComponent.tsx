3874de5c2bdc3d99341c0d82ae4a9b690d0e7a6c
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2024-08-07 01:10:37 +0200

    hint button style

--- src/components/CharacterComponent.tsx
+++ src/components/CharacterComponent.tsx
@@ -1,7 +1,22 @@
+import { useContext, useEffect, useState } from "react";
+import { ArgumentContext } from "../contexts/ArgumentContextProvider";
+import HintComponent from "./HintComponent";
+
 const CharacterComponent = (character: Character) => {
+  const { act } = useContext(ArgumentContext);
+  const [currentHint, setCurrentHint] = useState<Hint>();
+
+  useEffect(() => {
+    if (character.hints[act] && character.hints[act].length > 0)
+      setCurrentHint(character.hints[act][0]);
+  }, [act]);
+
   return (
     <div className="character">
-      {character.title} {character.name} {character.surname}
+      <div>
+        {character.title} {character.name} {character.surname}
+      </div>
+      <HintComponent hint={currentHint} />
     </div>
   );
 };
--- src/components/HintComponent.tsx
+++ src/components/HintComponent.tsx
@@ -1,9 +1,16 @@
-const HintComponent = (hint: Hint) => {
+interface HintProps {
+  hint?: Hint;
+}
+
+const HintComponent = (props: HintProps) => {
+  const { hint } = props;
   return (
     <div>
-      <div>type: {hint.type}</div>
-      <div>name: {hint.name}</div>
-      <button>Give Hint</button>
+      {hint ? <div>type: {hint.type}</div> : undefined}
+      {hint ? <div>type: {hint.name}</div> : undefined}
+      <div className="container">
+        {hint ? <div className="button">Give Hint</div> : undefined}
+      </div>
     </div>
   );
 };
